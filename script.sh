4758aa07ab9285cba041bbfa7cde9e7bbf5f4d34
alquama00s <alquama00s@outlook.com>
2022-02-10 20:10:59 +0530

    creation added

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 help(){
     echo "'use bash ./script.sh -d 2' to mark problem 2 done"
     echo "'use bash ./script.sh -n 2' to mark problem 2 not done"
+    echo "'use bash ./script.sh -c 2.prob-name' to create desired folder structure and open code"
+    echo "u can also set fraz variable to rood project path and the call this script from any location"
 }
 
 
@@ -12,13 +14,31 @@ nDone_(){
     sed -i "s/^|[[:space:]]*$1[[:space:]]*|[[:space:]]*:heavy_check_mark:[[:space:]]*|/|$1||/" ./readme.md
 }
 
-while getopts ":d:n:" opt; do
+create(){
+    this=`pwd`
+    if [[ $fraz != "" ]]
+    then
+        echo here
+        cd $fraz
+    fi
+    mkdir $1
+    cd ./$1
+    touch Solution.java
+    echo "public class Solution{}" >> ./Solution.java
+    code .
+    cd $this
+}
+
+while getopts ":d:n:c:" opt; do
     case $opt in
         d) serial="$OPTARG"
-        done_ $serial
+            done_ $serial
         ;;
         n) serial="$OPTARG"
-        nDone_ $serial
+            nDone_ $serial
+        ;;
+        c) name="$OPTARG"
+            create $name
         ;;
         \?) help
         exit 1;;
