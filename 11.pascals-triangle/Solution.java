8b2f8262cfc151b605642116db28472164325b94
alquama00s <alquama00s@outlook.com>
2022-02-22 11:22:30 +0530

    p11

--- 11.pascals-triangle/Solution.java
+++ 11.pascals-triangle/Solution.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         ArrayList<List<Integer>> ans = new ArrayList<List<Integer>>();
         List<List<Integer>> lst = new ArrayList<List<Integer>>();
-        ArrayList<Integer> temp = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
             ans.add(new ArrayList<>());
             ans.get(i).add(1);
@@ -18,13 +18,16 @@ class Solution {
                 ans.get(j).add(ans.get(j - 1).get(i - j) + ans.get(j).get(i - j - 1));
             }
         }
+        //System.out.println(ans);
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j <= i; j++) {
                 temp.add(ans.get(j).get(i - j));
             }
-            lst.add(temp);
+            lst.add(new ArrayList<>(temp));
             temp.clear();
         }
         return lst;
     }
 }
+
+
