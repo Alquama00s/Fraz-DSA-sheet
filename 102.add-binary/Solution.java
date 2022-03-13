95b7e572ec0bac763fee19509eb09cb7e3bd1b64
alquama00s <alquama00s@outlook.com>
2022-03-13 16:30:46 +0530

    p102

--- 102.add-binary/Solution.java
+++ 102.add-binary/Solution.java
@@ -1,39 +1,37 @@
 class Solution {
-    public static void main(String[] args) {
-        System.out.println(new Solution().addBinary("11", "1"));
-    }
-    char[] add(char a,char b,char c){
-        int _a=Integer.parseInt(""+a);
-        int _b=Integer.parseInt(""+b);
-        int _c=Integer.parseInt(""+c);
-        char add=((_a^_b^_c)+"").charAt(0);
-        char car=((_a&(_b|_c))+"").charAt(0);
-        return new char[]{add,car};
-    }
     public String addBinary(String a, String b) {
-        int i = a.length()-1;
-        int j= b.length()-1;
-        char ca,cb,cc='0';
-        char[] tempAns;
-        String ans="";
-        while(i>=0||j>=0){
-            if(i<0){
-                ca='0';
-            }else{
-                ca=a.charAt(i);
-            }
-            if(j<0){
-                cb='0';
-            }else{
-                cb=b.charAt(j);
-            }
-            tempAns=add(ca,cb,cc);
-            //System.out.println(tempAns[0]+" "+tempAns[1]);
-            ans=(""+tempAns[0])+ans;
-            cc=tempAns[1];
-            i--;j--;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        char cc = '0';
+        int _a, _b, _c;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 && j >= 0) {
+            _a = a.charAt(i) - 48;
+            _b = b.charAt(j) - 48;
+            _c = cc - 48;
+            ans.insert(0,(char) ((_a ^ _b ^ _c) + 48));
+            cc = (char) (((_a | _b) & (_b | _c) & (_c | _a)) + 48);
+            i--;
+            j--;
         }
-        ans=(""+cc)+ans;
-        return ans;
+        while (i >= 0) {
+            _a = a.charAt(i) - 48;
+            _b = 0;
+            _c = cc - 48;
+            ans.insert(0,(char) ((_a ^ _b ^ _c) + 48));
+            cc = (char) (((_a | _b) & (_b | _c) & (_c | _a)) + 48);
+            i--;
+        }
+        while (j >= 0) {
+            _a = 0;
+            _b = b.charAt(j)-48;
+            _c = cc - 48;
+            ans.insert(0,(char) ((_a ^ _b ^ _c) + 48));
+            cc = (char) (((_a | _b) & (_b | _c) & (_c | _a)) + 48);
+            j--;
+        }
+        if (cc == '1')
+            ans.insert(0,cc);
+        return ans.toString();
     }
 }
