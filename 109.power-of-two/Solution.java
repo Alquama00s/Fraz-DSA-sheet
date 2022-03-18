83f6ba5ff3ec42f2899a02a2deea4d8ef094e225
alquama00s <alquama00s@outlook.com>
2022-03-18 13:53:04 +0530

    p109

--- 109.power-of-two/Solution.java
+++ 109.power-of-two/Solution.java
@@ -1,12 +1,5 @@
-
 class Solution {
-    public static void main(String[] args) {
-        System.out.println(new Solution().isPowerOfTwo(
-                16777217));
-    }
-
     public boolean isPowerOfTwo(int n) {
-        System.out.println((Math.log(n) / Math.log(2)) + " " + (int) (Math.log(n) / Math.log(2)));
-        return ((Math.log(n) / Math.log(2)) - (int) (Math.log(n) / Math.log(2))) < 0.000000000000001d;
+        return n>0 & (n & (n - 1)) == 0;
     }
 }
